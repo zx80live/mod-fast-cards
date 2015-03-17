08d36a53159d5df02b8c515abc29af636c893e0a
Andrew Proshkin <zx80live@gmail.com>
2015-03-17 18:05:12 +0300

    #

--- src/main/scala/com/zx80live/mod/fastcards/OldCard.scala
+++
@@ -1,7 +0,0 @@
-package com.zx80live.mod.fastcards
-
-@deprecated
-case class OldCard(value: String, kind: Option[String] = None, translations: List[String], examples: List[OldExample] = Nil, transcript: Option[String] = None, var passCount: Int = 0, var times: List[Long] = Nil)
-
-@deprecated
-case class OldExample(text: String, translations: List[String] = Nil)
