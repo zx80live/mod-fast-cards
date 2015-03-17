75af1e8a46a14d53bf56c80dcb80eb125f376b2c
Andrew Proshkin <zx80live@gmail.com>
2015-03-17 23:56:36 +0300

    #

--- src/main/scala/com/zx80live/mod/fastcards/ExamController.scala
+++ src/main/scala/com/zx80live/mod/fastcards/ExamController.scala
@@ -1,11 +1,31 @@
 package com.zx80live.mod.fastcards
 
 import scala.tools.jline.console.{ConsoleReader => R}
-import scala.util.{Failure, Success}
 
 object ExamController extends ExamExtensions with ExamFSM with ArgumentParser {
 
   import com.zx80.mod.util.console.ConsoleCSS._
+
+  object Renderers {
+
+    def renderStartExam(badFilePrefixOpt: Option[String]): String =
+      "\nstart exam".attr(Format.Bold | Foreground.color(70)) + badFilePrefixOpt.map(_ => "").getOrElse(" " + "repeat bad".attr(Foreground.color(22))) + "\n"
+
+    def renderExamples(xs: List[Example]): String =
+      xs.map(e => "* " + e.text.trim).mkString("|").attr(Foreground.color(107))
+
+    def renderTranslations(xs: List[String]): String =
+      xs.mkString("|").attr(Foreground.color(103))
+
+    def renderCardValue(d: Data): String = d.value.attr(Format.Bold | Foreground.Cyan) +
+      d.transcript.map(t => ("[" + t + "]").foreground(24)).getOrElse("") + " " +
+      d.kind.getOrElse("").attr(Foreground.Yellow)
+
+    def renderNoneCard: String = "<none>".attr(Foreground.Yellow)
+  }
+
+
+  import com.zx80live.mod.fastcards.ExamController.Renderers._
 
 
   def main(args: Array[String]): Unit = parseArgs(args).map { config =>
@@ -15,7 +35,7 @@ object ExamController extends ExamExtensions with ExamFSM with ArgumentParser {
       val badMode = config.files.map(getFileExtension).collect { case Some("bad") | Some("mid") => true}.length > 0
       val badFilePrefixOpt: Option[String] = if (!badMode) Some(config.files.map(_.getName).mkString("_")) else None
 
-      println("\nstart exam".attr(Format.Bold | Foreground.color(70)) + badFilePrefixOpt.map(_ => "").getOrElse(" " + "repeat bad".attr(Foreground.color(22))) + "\n")
+      println(renderStartExam(badFilePrefixOpt))
 
       val state: Deck = exam(cards)
       //todo write state
@@ -31,10 +51,10 @@ object ExamController extends ExamExtensions with ExamFSM with ArgumentParser {
       clearLine()
 
       print("\r" + d.current.map {
-        case c: InfoSide => c.data.examples.map(e => "* " + e.text).mkString("|")
-        case c: BackSide => c.data.translations.mkString("|")
-        case c: Card => c.data.value
-      }.getOrElse("<none>") + "                      ")
+        case c: InfoSide => renderExamples(c.data.examples)
+        case c: BackSide => renderTranslations(c.data.translations)
+        case c: Card => renderCardValue(c.data)
+      }.getOrElse(renderNoneCard))
     }
 
 
