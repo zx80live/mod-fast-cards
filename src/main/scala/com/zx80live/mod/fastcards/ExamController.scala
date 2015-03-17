db47bc8805e5f0f51539a859bdc67aac39bdea99
Andrew Proshkin <zx80live@gmail.com>
2015-03-18 00:02:24 +0300

    #

--- src/main/scala/com/zx80live/mod/fastcards/ExamController.scala
+++ src/main/scala/com/zx80live/mod/fastcards/ExamController.scala
@@ -1,41 +1,23 @@
 package com.zx80live.mod.fastcards
 
+import java.io.File
+
 import scala.tools.jline.console.{ConsoleReader => R}
 
 object ExamController extends ExamExtensions with ExamFSM with ArgumentParser {
 
   import com.zx80.mod.util.console.ConsoleCSS._
-
-  object Renderer {
-
-    def renderStartExam(badFilePrefixOpt: Option[String]): String =
-      "\nstart exam".attr(Format.Bold | Foreground.color(70)) + badFilePrefixOpt.map(_ => "").getOrElse(" " + "repeat bad".attr(Foreground.color(22))) + "\n"
-
-    def renderExamples(xs: List[Example]): String =
-      xs.map(e => "* " + e.text.trim).mkString("|").attr(Foreground.color(107))
-
-    def renderTranslations(xs: List[String]): String =
-      xs.mkString("|").attr(Foreground.color(103))
-
-    def renderCardValue(d: Data): String = d.value.attr(Format.Bold | Foreground.Cyan) +
-      d.transcript.map(t => ("[" + t + "]").foreground(24)).getOrElse("") + " " +
-      d.kind.getOrElse("").attr(Foreground.Yellow)
-
-    def renderNoneCard: String = "<none>".attr(Foreground.Yellow)
-  }
-
-
   import com.zx80live.mod.fastcards.ExamController.Renderer._
 
 
   def main(args: Array[String]): Unit = parseArgs(args).map { config =>
-    println("\n" + config.files.map(_.getName).mkString(", ").attr(Foreground.color(237)))
+    renderFiles(config.files)
 
     readCards(config).map { cards =>
       val badMode = config.files.map(getFileExtension).collect { case Some("bad") | Some("mid") => true}.length > 0
       val badFilePrefixOpt: Option[String] = if (!badMode) Some(config.files.map(_.getName).mkString("_")) else None
 
-      println(renderStartExam(badFilePrefixOpt))
+      renderStartExam(badFilePrefixOpt)
 
       val state: Deck = exam(cards)
       //todo write state
@@ -67,6 +49,29 @@ object ExamController extends ExamExtensions with ExamFSM with ArgumentParser {
   }
 
 
-  def clearLine(): Unit = print("\r" + (for (i <- 0 until 155) yield " ").mkString(""))
+  object Renderer {
+    val consoleWidth = 155
+
+    def renderFiles(xs: Seq[File]): Unit =
+      println("\n" + xs.map(_.getName).mkString(", ").attr(Foreground.color(237)))
+
+    def renderStartExam(badFilePrefixOpt: Option[String]): Unit =
+      println("\nstart exam".attr(Format.Bold | Foreground.color(70)) + badFilePrefixOpt.map(_ => "").getOrElse(" " + "repeat bad".attr(Foreground.color(22))) + "\n")
+
+    def renderExamples(xs: List[Example]): String =
+      xs.map(e => "* " + e.text.trim).mkString("|").attr(Foreground.color(107))
+
+    def renderTranslations(xs: List[String]): String =
+      xs.mkString("|").attr(Foreground.color(103))
+
+    def renderCardValue(d: Data): String = d.value.attr(Format.Bold | Foreground.Cyan) +
+      d.transcript.map(t => ("[" + t + "]").foreground(24)).getOrElse("") + " " +
+      d.kind.getOrElse("").attr(Foreground.Yellow)
+
+    def renderNoneCard: String = "<none>".attr(Foreground.Yellow)
+
+    def clearLine(): Unit = print("\r" + (for (i <- 0 until consoleWidth) yield " ").mkString(""))
+  }
+
 
 }
