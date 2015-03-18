8ba1642aac5c1010a5e91f43ee40a3a494ef3cb5
Andrew Proshkin <zx80live@gmail.com>
2015-03-18 23:20:56 +0300

    #

--- src/main/scala/com/zx80live/mod/fastcards/ExamController.scala
+++ src/main/scala/com/zx80live/mod/fastcards/ExamController.scala
@@ -29,91 +29,6 @@ object ExamController extends ExamExtensions with ArgumentParser {
     val con = new R()
     var state = Deck(cards)
 
-    def printState(d: Deck): Unit = {
-      clearLine()
-
-      print("\r" + d.current.map {
-        case c: InfoSide => renderExamples(c.data.examples)
-        case c: BackSide => renderTranslations(c.data.translations)
-        case c: Card => renderCardValue(c.data)
-      }.getOrElse(renderNoneCard))
-    }
-
-
-
-    object Actions {
-
-      object Code {
-        val LEFT = 2
-        val RIGHT = 6
-        val SPACE = 32
-        val ENTER = 10
-        val I = 105
-        val S = 115
-        val D = 100
-        val CTRL_D = scala.tools.jline.console.Key.CTRL_D.code
-      }
-
-      case class Event(code: Int, state: Deck)
-
-      implicit val passLimit: Int = 2
-
-      val caseRight: PartialFunction[Event, Deck] = {
-        case Event(Code.RIGHT, s) => s.resetCurrent.next
-      }
-
-      val caseLeft: PartialFunction[Event, Deck] = {
-        case Event(Code.LEFT, s) => s.resetCurrent.prev
-      }
-
-      val caseSpace: PartialFunction[Event, Deck] = {
-        case Event(Code.SPACE, s) =>
-          s.current match {
-            case Some(c: BackSide) =>
-              s.estimateFalse
-            case _ => s.backCurrent
-          }
-      }
-
-      val caseEnter: PartialFunction[Event, Deck] = {
-        case Event(Code.ENTER, s) =>
-          s.current match {
-            case Some(c: BackSide) =>
-              s.estimateTrue(0L)
-
-            case _ => s.backCurrent
-          }
-      }
-
-      val caseInfo: PartialFunction[Event, Deck] = {
-        case Event(Code.I, s) =>
-          s.current match {
-            case Some(c: InfoSide) => s.reverseCurrent
-            case _ => s.infoCurrent
-          }
-      }
-
-      val caseStatistic: PartialFunction[Event, Deck] = {
-        case Event(Code.S, s) => s
-      }
-
-      val caseDrop: PartialFunction[Event, Deck] = {
-        case Event(Code.D, s) => s.drop
-      }
-
-      val caseDropAll: PartialFunction[Event, Deck] = {
-        case Event(Code.CTRL_D, s) => s.dropAll
-      }
-
-      val wildcard: PartialFunction[Event, Deck] = {
-        case Event(_, s) => s
-      }
-
-      def transition = caseRight orElse caseLeft orElse
-        caseSpace orElse caseEnter orElse
-        caseInfo orElse caseStatistic orElse caseDrop orElse caseDropAll orElse wildcard
-    }
-
     import Actions.{Event, transition}
 
     while (!state.isInstanceOf[EmptyStock]) {
@@ -122,6 +37,79 @@ object ExamController extends ExamExtensions with ArgumentParser {
       state = transition(Event(con.readVirtualKey(), state))
     }
     state
+  }
+
+  object Actions {
+
+    object Code {
+      val LEFT = 2
+      val RIGHT = 6
+      val SPACE = 32
+      val ENTER = 10
+      val I = 105
+      val S = 115
+      val D = 100
+      val CTRL_D = scala.tools.jline.console.Key.CTRL_D.code
+    }
+
+    case class Event(code: Int, state: Deck)
+
+    implicit val passLimit: Int = 2
+
+    val caseRight: PartialFunction[Event, Deck] = {
+      case Event(Code.RIGHT, s) => s.resetCurrent.next
+    }
+
+    val caseLeft: PartialFunction[Event, Deck] = {
+      case Event(Code.LEFT, s) => s.resetCurrent.prev
+    }
+
+    val caseSpace: PartialFunction[Event, Deck] = {
+      case Event(Code.SPACE, s) =>
+        s.current match {
+          case Some(c: BackSide) =>
+            s.estimateFalse
+          case _ => s.backCurrent
+        }
+    }
+
+    val caseEnter: PartialFunction[Event, Deck] = {
+      case Event(Code.ENTER, s) =>
+        s.current match {
+          case Some(c: BackSide) =>
+            s.estimateTrue(0L)
+
+          case _ => s.backCurrent
+        }
+    }
+
+    val caseInfo: PartialFunction[Event, Deck] = {
+      case Event(Code.I, s) =>
+        s.current match {
+          case Some(c: InfoSide) => s.reverseCurrent
+          case _ => s.infoCurrent
+        }
+    }
+
+    val caseStatistic: PartialFunction[Event, Deck] = {
+      case Event(Code.S, s) => s
+    }
+
+    val caseDrop: PartialFunction[Event, Deck] = {
+      case Event(Code.D, s) => s.drop
+    }
+
+    val caseDropAll: PartialFunction[Event, Deck] = {
+      case Event(Code.CTRL_D, s) => s.dropAll
+    }
+
+    val wildcard: PartialFunction[Event, Deck] = {
+      case Event(_, s) => s
+    }
+
+    def transition = caseRight orElse caseLeft orElse
+      caseSpace orElse caseEnter orElse
+      caseInfo orElse caseStatistic orElse caseDrop orElse caseDropAll orElse wildcard
   }
 
 
@@ -147,6 +135,16 @@ object ExamController extends ExamExtensions with ArgumentParser {
     def renderNoneCard: String = "<none>".attr(Foreground.Yellow)
 
     def clearLine(): Unit = print("\r" + (for (i <- 0 until consoleWidth) yield " ").mkString(""))
+
+    def printState(d: Deck): Unit = {
+      clearLine()
+
+      print("\r" + d.current.map {
+        case c: InfoSide => renderExamples(c.data.examples)
+        case c: BackSide => renderTranslations(c.data.translations)
+        case c: Card => renderCardValue(c.data)
+      }.getOrElse(renderNoneCard))
+    }
   }
 
 
