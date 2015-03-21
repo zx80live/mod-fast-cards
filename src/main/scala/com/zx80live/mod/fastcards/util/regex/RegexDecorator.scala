dd6650683bc347c1308e0798252d883185564b03
Andrew Proshkin <zx80live@gmail.com>
2015-03-21 16:21:06 +0300

    #

--- src/main/scala/com/zx80live/mod/fastcards/util/regex/RegexDecorator.scala
+++ src/main/scala/com/zx80live/mod/fastcards/util/regex/RegexDecorator.scala
@@ -1,9 +1,22 @@
 package com.zx80live.mod.fastcards.util.regex
 
+import java.util.regex.{Matcher, Pattern}
+
 trait RegexDecorator {
 
   implicit class StringDecorateExtensions(s: String) {
-    def decorateAll(pattern: String, left: String, right: String): String = s
+    def decorateAll(pattern: String, left: String, right: String): String = {
+      val p: Pattern = Pattern.compile(pattern)
+      val m: Matcher = p.matcher(s)
+      val sb = new StringBuffer()
+
+      while (m.find()) {
+        m.appendReplacement(sb, left + m.group(1) + right)
+      }
+      m.appendTail(sb)
+      sb.toString
+    }
+
   }
 
 }
