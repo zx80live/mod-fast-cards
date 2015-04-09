4e65ece463116ce3e7d30ec3a45ae91e4d4386ac
Andrew Proshkin <zx80live@gmail.com>
2015-04-09 20:13:56 +0300

    #

--- src/main/scala/com/zx80live/mod/fastcards/Exam.scala
+++ src/main/scala/com/zx80live/mod/fastcards/Exam.scala
@@ -10,7 +10,7 @@ trait Exam {
     def isBack: Boolean = !isFront
   }
 
-  case class Deck(stock: List[Card], estimated: List[Card] = Nil, discard: List[Card] = Nil)
+  case class Deck(stock: List[Card], passes: Int, estimated: List[Card] = Nil, discard: List[Card] = Nil)
 
   implicit class CardExtensions(c: Card) {
     def backSide: Card = if (c.isFront) c.copy(isFront = false) else c
@@ -38,9 +38,9 @@ trait Exam {
       d.copy(stock = d.stock.tail, discard = c :: d.discard)
     }
 
-    def estimateCurrent(time: Option[Time] = None)(implicit passCount: Int): Option[Deck] = current.map { c =>
+    def estimateCurrent(time: Option[Time] = None): Option[Deck] = current.map { c =>
       val estimated = c.estimate(time)
-      if (estimated.estimates.length < passCount) {
+      if (estimated.estimates.count(_.nonEmpty) < d.passes) {
         // estimate
         d.copy(stock = d.stock.tail, estimated = estimated :: d.estimated)
       } else {
@@ -55,6 +55,5 @@ trait Exam {
       Some(if (shuffle) d.copy(stock = Random.shuffle(d.estimated)) else d.copy(stock = d.estimated))
     } else None
   }
-
 
 }
